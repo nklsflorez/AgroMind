8e0f4c038392ca3282678fbafb294181a5812162
BUIVSC02\Admin <Nikolas>
2025-11-15 17:50:29 -0500

    Trabajo de hoy

---
+++ proyectofinal.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  9 09:58:02 2025
+
+@author: Admin
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df_xls = pd.read_excel("anex-SIPSALeche-SerieHistoricaPrecios-2025.xlsx")
+print(df_xls)
+
+
